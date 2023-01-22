469074b7fe2bef0c54e83c08b6c67461466d3401
al-imam <alimam01828@gmail.com>
2023-01-22 17:26:58 +0600

    init by vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import classes from "./app.module.css";
+
+function App() {
+  return (
+    <div className={classes.app}>
+      <div className={classes.center}></div>
+    </div>
+  );
+}
+
+export default App;
