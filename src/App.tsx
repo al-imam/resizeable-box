73a1be95cde56209cfca33e0a56eff4467260731
al-imam <alimam01828@gmail.com>
2023-01-23 21:34:11 +0600

    add text and state to manipulate text

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 import classes from "./app.module.css";
 
 function App() {
+  const [expand, setExpand] = useState(false);
   return (
     <div className={classes.app}>
       <div className={classes.box}>
         <h1>Hello</h1>
-        <button>Toggle</button>
+        <button onClick={() => setExpand(!expand)}>Toggle</button>
+        <div>
+          {expand ? (
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+              ipsum in ducimus fugit dolore reiciendis reprehenderit corrupti
+              molestiae illum facilis!
+            </p>
+          ) : (
+            <p>Lorem, ipsum dolor.</p>
+          )}
+        </div>
       </div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam nihil laborum
+      dolore recusandae hic. Enim vero repellat molestias modi labore voluptas
+      totam voluptatem, quae, eveniet iusto aliquam reprehenderit exercitationem
+      laborum?
     </div>
   );
 }
