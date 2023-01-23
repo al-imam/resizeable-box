d6be61a1cad466c4187a83753ee69ec327c99fb1
al-imam <alimam01828@gmail.com>
2023-01-23 22:21:19 +0600

    do some magic
      - i do't know what i does but it works

--- src/ResizeAble.tsx
+++ src/ResizeAble.tsx
@@ -2,12 +2,30 @@ import { motion } from "framer-motion";
 import React from "react";
 import useMeasure from "react-use-measure";
 
+const circular = () => {
+  const seen = new WeakSet();
+  return (key: any, value: any) => {
+    if (key.startsWith("_")) return; // Don't compare React's internal props.
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) return;
+      seen.add(value);
+    }
+    return value;
+  };
+};
+
 function ResizeAble({ children }: { children: React.ReactNode }) {
   const [ref, { height }] = useMeasure();
 
   return (
     <motion.div animate={{ height }}>
-      <div ref={ref}>{children}</div>
+      <motion.div
+        key={JSON.stringify(children, circular())}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+      >
+        <div ref={ref}>{children}</div>
+      </motion.div>
     </motion.div>
   );
 }
